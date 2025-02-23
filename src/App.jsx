fda5be7fd492ccf95190e209c2d2d1cb623ef8a8
Vladimir Stoikov <vspsmoke@gmail.com>
2025-02-23 17:06:18 +0300

    feat: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
 function App() {
 
   return (
    <>
-    <MovieCard movie={{title: "Test film", releaseDate: "2025"}}/>
-    <MovieCard movie={{title: "Test film 2", releaseDate: "2026"}}/>
+    <Home />
    </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import MovieCard from "../components/MovieCard"
+
+const URLS = {
+  oneplusone: "https://avatars.mds.yandex.net/get-kinopoisk-image/10900341/caf9f155-1a19-42f1-a0f3-9c8773e9083e/72x108",
+  interstellar: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/430042eb-ee69-4818-aed0-a312400a26bf/72x108",
+  theShawshankRedemption: "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0b76b2a2-d1c7-4f04-a284-80ff7bb709a4/72x108"
+}
+
+export default function Home() {
+
+  const moviesList = [
+    { id: 1, url: URLS.oneplusone, title: "1+1", releaseDate: "2011" },
+    { id: 2, url: URLS.interstellar, title: "Interstellar", releaseDate: "2014" },
+    { id: 3, url: URLS.theShawshankRedemption, title: "The Shawshank Redemption", releaseDate: "1994" },
+  ];
+
+  function handleSearch(e) {
+    e.preventDefault();
+    console.log('search');
+  }
+
+  return (
+    <div className="home">
+      <form className="search-form" onSubmit={event => handleSearch(event)}>
+        <input className="search-input" type="text" placeholder="Search for movies..." />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="movies-grid">
+        {moviesList.map(movie => (
+          <MovieCard key={movie.id} movie={movie}/>
+        ))}
+      </div>
+    </div>
+  )
+}
