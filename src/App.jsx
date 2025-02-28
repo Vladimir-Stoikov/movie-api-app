87b6abc6d5ce2ba4b71cdcc56f3d1429f00740e7
Vladimir Stoikov <vspsmoke@gmail.com>
2025-02-28 22:04:07 +0300

    check api key dont go to github

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import './css/App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import NavBar from './components/NavBar'
+import apiKey from '../config'
+
 
 function App() {
+
+  console.log(apiKey);
 
   return (
   <div>
