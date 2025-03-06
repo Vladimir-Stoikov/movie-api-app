48d7d68ba14c74faedeb34a59e96155532060ca8
Vladimir Stoikov <vspsmoke@gmail.com>
2025-03-06 21:47:08 +0300

    fix: another change api, try amdb, fixing issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,7 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard"
 import "../css/Home.css"
-import { getMoviesList, searchMovies } from "../services/apiHandler";
-
-const URLS = {
-  oneplusone: "https://avatars.mds.yandex.net/get-kinopoisk-image/10900341/caf9f155-1a19-42f1-a0f3-9c8773e9083e/72x108",
-  interstellar: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/430042eb-ee69-4818-aed0-a312400a26bf/72x108",
-  theShawshankRedemption: "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0b76b2a2-d1c7-4f04-a284-80ff7bb709a4/72x108"
-}
+import { getPopularMovies, searchMovies } from "../services/apiHandler";
 
 export default function Home() {
 
@@ -16,16 +10,10 @@ export default function Home() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true)
 
-  const moviesList = [
-    { id: 1, url: URLS.oneplusone, title: "1+1", releaseDate: "2011" },
-    { id: 2, url: URLS.interstellar, title: "Interstellar", releaseDate: "2014" },
-    { id: 3, url: URLS.theShawshankRedemption, title: "The Shawshank Redemption", releaseDate: "1994" },
-  ];
-
   useEffect(() => {
     const loadData = async () => {
       try {
-        const moviesData = await getMoviesList();
+        const moviesData = await getPopularMovies();
         setMovies(moviesData.items);
       } catch(err) {
         setError("Failed to load...", err);
@@ -36,8 +24,6 @@ export default function Home() {
 
     loadData();
   }, [])
-
-  console.log(movies);
 
   async function handleSearch(e) {
     e.preventDefault();
@@ -54,7 +40,6 @@ export default function Home() {
     } finally {
       setLoading(false);
     }
-
   }
 
   
@@ -70,11 +55,10 @@ export default function Home() {
       {loading ? <div className="loading">Loading...</div> : 
 
       <div className="movies-grid">
-        {movies.map(movie => (
-          movie.nameRu && movie.nameRu.toLowerCase().startsWith(searchQuery) && <MovieCard key={movie.kinopoiskId} movie={{id: movie.kinopoiskId, url: movie.posterUrl, title: movie.nameRu, releaseDate: movie.year}}/>
-          // <div>{movie.nameRu}</div>
-        ))}
-      </div>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
       }
     </div>
   )
--- src/services/apiHandler.js
+++ src/services/apiHandler.js
@@ -1,28 +1,15 @@
-import apiKey from '../../config';
+import API_KEY from '../../config';
 
-// import { API_KEY, BASE_URL } from '../../config';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-export async function getMoviesList() {
-  const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films', {
-    method: 'GET',
-    headers: {
-      'X-API-KEY': `${apiKey}`,
-      'Content-Type': 'application/json',
-    },
-  });
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
-  return data;
-}
+  return data.results;
+};
 
-export async function searchMovies(str) {
-  console.log(str);
-  const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=%D1%82%D0%B5%D1%80%D0%BC%D0%B8%D0%BD%D0%B0%D1%82%D0%BE%D1%80&page=1', {
-    method: 'GET',
-    headers: {
-      'X-API-KEY': `${apiKey}`,
-      'Content-Type': 'application/json',
-    },
-  });
+export const searchMovies = async query => {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
   const data = await response.json();
-  return data;
-}
+  return data.results;
+};
