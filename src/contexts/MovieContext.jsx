e9ccbf905b7ad8c9448eb5bebdd27bce4731cba8
Vladimir Stoikov <vspsmoke@gmail.com>
2025-03-10 21:38:15 +0300

    feat: comleted context component, added functions to control store

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -8,13 +8,39 @@ export const MovieProvider = ({children}) => {
 
   const [favorites, setFavorites] = useState([]);
 
+  const value = {
+    favorites,
+    addToFavorites,
+    removeFromFavorites,
+    isFavorite
+  }
+
   useEffect(() => {
     const storedFavs = sessionStorage.getItem("favorites");
 
-    if(storedFavs) setFavorites(JSON.parse(storeFavs));
+    if(storedFavs) setFavorites(JSON.parse(storedFavs));
   }, [])
 
-  return <MovieContext.Provider>
+  useEffect(() => {
+    sessionStorage.setItem('favorites', JSON.stringify(favorites));
+  
+  }, [favorites])
+  
+  function addToFavorites(movie) {
+    setFavorites(prev => [...prev, movie]);
+  }
+
+  function removeFromFavorites(movieId) {
+    setFavorites(prev => prev.filter(movie => movie.id !== movieId));
+  }
+
+  function isFavorite(movieId) {
+    return favorites.some(movie => movie.id === movieId);
+  }
+
+
+
+  return <MovieContext.Provider value={value}>
     {children}
   </MovieContext.Provider>
 }
