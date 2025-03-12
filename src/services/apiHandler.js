08c4a8d08e72ea940c89e8e0492efc97ad325e33
Vladimir Stoikov <vspsmoke@gmail.com>
2025-03-12 21:20:34 +0300

    docs(readme): change description, refactor(api): added your_api const for user api

--- src/services/apiHandler.js
+++ src/services/apiHandler.js
@@ -2,14 +2,18 @@ import API_KEY from '../../config';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+const YOUR_API = 'Write your API here';
+
+const API_KEY_VAR = API_KEY ? API_KEY : YOUR_API;
+
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY_VAR}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async query => {
-  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY_VAR}&query=${encodeURIComponent(query)}`);
   const data = await response.json();
   return data.results;
 };
